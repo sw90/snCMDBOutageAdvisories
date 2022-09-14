6a7dc1c821d98d60b947a6e10fdfe5ddd0ae0461
sw90 <sw@ml1.net>
2022-09-14 19:32:44 +0100

    Create catalog_script_client.outage_advisory.js

---
+++ catalog_script_client.outage_advisory.js
@@ -0,0 +1,32 @@
+function onChange(control, oldValue, newValue, isLoading) {
+    if (isLoading || newValue == '') {
+        return;
+    }
+
+    function processOutageInfo(serverResponse) {
+        var answer = serverResponse.responseXML.documentElement.getAttribute('answer');
+        var outageInfo = JSON.parse(answer);
+        var msg = '';
+        if (outageInfo.type) {
+            if (outageInfo.task) {
+                msg += outageInfo.task + ": ";
+            }
+            msg += outageInfo.advisory;
+            if (outageInfo.end) {
+                msg += " Planned end time: " + outageInfo.end + ".";
+            }
+        }
+        if (outageInfo.message) {
+            msg += " " + outageInfo.message;
+        }
+        if (msg) {
+            g_form.showFieldMsg('cmdb_ci', msg, 'warning');
+        }
+    }
+
+    var ga = new GlideAjax('global.CMDBOutage');
+    ga.addParam('sysparm_name', 'checkExistingOutage');
+    ga.addParam('sysparm_id', newValue);
+    ga.getXML(processOutageInfo);
+
+}
